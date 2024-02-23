01a0371b6bf38faf0108c29db28b61133f7b45bd
StepashkaJu <yuliyanosova0807@gmail.com>
2024-02-23 22:20:42 +0400

    add fetch, app.js, update desktop.css, index.html

---
+++ app.js
@@ -0,0 +1,15 @@
+fetch('.//data.json')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(element => {
+
+            let estimationQuality = document.getElementById(element.category);
+            let iconDiv = estimationQuality.getElementsByClassName("rectangle-icon")[0];
+            iconDiv.insertAdjacentHTML("beforeend", `<img src="${element.icon}" />`);
+            let resultDiv = estimationQuality.getElementsByClassName("rectangle-result")[0];
+            resultDiv.innerHTML = element.score;
+            let qualityDiv = estimationQuality.getElementsByClassName("rectangle-name")[0];
+            qualityDiv.innerHTML = element.category;
+            console.log(element)
+        })
+    });
